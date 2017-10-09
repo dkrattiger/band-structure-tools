ef81d342d08de543d2181ad94331288fcabebb31
Dimitri Krattiger <dimitri.krattiger@gmail.com>
2017-10-09 15:18:20 +0200

    Added residual-enhanced solver options into k(w) dispersion solver code

--- dispersion_solver_k_w.m
+++ dispersion_solver_k_w.m
@@ -1,4 +1,4 @@
-function [kappa,PHI,varargout] = dispersion_solver_k_w(omegas,Kf,Cf,Mf,dof_sets,R,n_curves)
+function [kappa,PHI,varargout] = dispersion_solver_k_w(omegas,Kf,Cf,Mf,dof_sets,R,varargin)
 
 % Dimitri Krattiger
 %
@@ -36,23 +36,58 @@ function [kappa,PHI,varargout] = dispersion_solver_k_w(omegas,Kf,Cf,Mf,dof_sets,
 %
 % t_wloop       = timing results for each frequency point (n_freq x 1)
 
-full_eig = false;
-if isempty(n_curves)
-    full_eig = true;
-elseif n_curves<0
-    full_eig = true;
+%% Default options
+% ======================================================================= %
+
+% if options does not exist, assume that single parameter n_curves has been
+% given
+if nargin<6
+    options.dummy = 0;
+else
+    if isstruct(varargin{1})
+        options = varargin{1};
+    else
+        options.n_curves = varargin{1};
+    end
 end
+
+% does the model have residual enhancement?
+resPlus = isstruct(Kf);
+
+% use dynamic reduction?
+dynRed = true;
+if isfield(options,'full_eig')
+    if ~options.full_eig    
+        dynRed = false;
+    end   
+end
+% don't use dynamic reduction if residual-enhanced model is detected
+if resPlus    
+    dynRed = false;
+end
+
+% default option values
+defaults.n_curves           = 10;
+defaults.verbose            = true;
+defaults.k                  = 30;   % number of Arnoldi vectors (increased values sometimes help with eigvalue conditioning)
+defaults.full_eig           = true;
+defaults.dynamicReduction   = dynRed;
+defaults.useConditionValue  = true;
+defaults.sparse             = true;
+
+% defaults.storephi = n_dof_per<10000 ;
+%defaults.fullEig = gets determined by model size and # curves;
+%defaults.fullIterativeEig = gets determined by model size and # curves;
+
+
+options = setstructfields(defaults,options);
 
 % guess1 = 'sm';
 guess1 = 1.1;
 guess2 = 0.;
 guess = guess1;
 
-% use dynamic reduction?
-dyn_red = true;
-if ~full_eig    
-    dyn_red = false;
-end
+
 
 %% Prepare for dynamic reduction
 % ======================================================================= %
@@ -73,9 +108,16 @@ n_b = length(i_b);
 
 %% Check whether damping matrix is included
 % ======================================================================= %
-use_C = true;
+% use_C = true;
 if isempty(Cf)
-    use_C = false;
+    clear Cf;
+    if resPlus
+        Cf.w0=0;
+        Cf.w2=0;
+        Cf.w4=0;
+    else
+        Cf = 0;
+    end
 end
 
 %% Form periodicity transformation
@@ -84,7 +126,7 @@ end
 % Output T_per is a structure with fields s0, s1, s2, s3, s12, s13, s23,
 % s123. These can be combined to form the Bloch-periodicity transformation
 % matrix for any wavevector.
-if dyn_red
+if options.dynamicReduction
     dof_sets.i = [];
 end
 T_per = Periodic_Boundary_Conditions(dof_sets);
@@ -98,7 +140,7 @@ n_p = size(R,2);
 n_om = length(omegas);
         
 % preallocate arrays
-if full_eig
+if options.full_eig
     n_curves = 2*n_dof_per;
 end
 PHI = zeros(n_dof_per,n_curves,n_om);
@@ -118,15 +160,26 @@ for j1 = 1:n_w
     % phase modification at boundaries due to wavevector
     w = omegas(j1);
     
-    % Form Dynamic matrix
-    if use_C
-        D = Kf+1i*w*Cf-w^2*Mf;
+    if resPlus
+        
+        % residual-enhanced representation
+        Kfe = Kf.w0 + w^2*(Kf.w2+(Kf.w2)') + w^4*Kf.w4;
+        Cfe = Cf.w0 + w^2*(Cf.w2+(Cf.w2)') + w^4*Cf.w4;
+        Mfe = Mf.w0 + w^2*(Mf.w2+(Mf.w2)') + w^4*Mf.w4;
+        
     else
-        D = Kf-w^2*Mf;
+        % standard representation
+        Kfe = Kf;
+        Cfe = Cf;
+        Mfe = Mf;
     end
     
+    % Form Dynamic matrix
+    D = Kfe + 1i*w*Cfe - w^2*Mfe;
+    D = (1/2)*(D+D');
+    
     % dynamic reduction
-    if dyn_red
+    if dynRed
         Dhat = D(i_b,i_b)-D(i_b,i_i)*(D(i_i,i_i)\D(i_i,i_b));
     else
         Dhat = D;
@@ -134,105 +187,79 @@ for j1 = 1:n_w
         
     % Apply periodicity transformation and decompose D by powers of 
     % e^(i kx Lx)
-    D0 = Tx'*Dhat*T0;
-    D1 = T0'*Dhat*T0 + Tx'*Dhat*Tx;
-    D2 = T0'*Dhat*Tx;    
+    D0 = Tx'*Dhat*T0;               % constant
+    D1 = T0'*Dhat*T0 + Tx'*Dhat*Tx; % linear in exp(i kx lx)
+    D2 = T0'*Dhat*Tx;               % quadratic in exp(i kx lx)
     
+    
+    % scaling value to improve conditioning of state space problem
+    if options.useConditionValue
+        condVal = (sum(diag(D0))+sum(diag(D1))+sum(diag(D2)))/(3*n_dof_per);
+    else
+        condVal = 1;
+    end
+        
+    % prepare for sparse represntation by finding indices of non-zero
+    % entries
     [D0row,D0col,D0val] = find(D0);
     [D1row,D1col,D1val] = find(D1);
     [D2row,D2col,D2val] = find(D2);
-    
+
     % sparse representation of state space matrices
     Arow = [D0row;((n_dof_per+1):(2*n_dof_per))'];
     Acol = [D0col;((n_dof_per+1):(2*n_dof_per))'];
-    Aval = [D0val;ones(n_dof_per,1)];
-    
+    Aval = [D0val;ones(n_dof_per,1)*condVal];
+
     Brow = [D1row;D2row;((n_dof_per+1):(2*n_dof_per))'];
     Bcol = [D1col;D2col+n_dof_per;(1:n_dof_per)'];
-    Bval = [-D1val;-D2val;ones(n_dof_per,1)];
-    
+    Bval = [-D1val;-D2val;ones(n_dof_per,1)*condVal];
+
     % form sparse state space matrices
     A = sparse(Arow,Acol,Aval,(2*n_dof_per),(2*n_dof_per));
     B = sparse(Brow,Bcol,Bval,(2*n_dof_per),(2*n_dof_per));
+
+%         % form state space matrices
+%         A = [D0,zeros(n_dof_per);zeros(n_dof_per),eye(n_dof_per)*condVal];
+%         B = [-D1,-D2;eye(n_dof_per)*condVal,zeros(n_dof_per)];
     
     % eigenvalue solution
-    if full_eig
+    if options.full_eig
         [PHIs,L] = eig(full(A),full(B),'vector');
     else
+        
+        % The matrices A and B should be sparse
         try
-            [PHIs,L] = eigs((A),(B),n_curves,guess);L = diag(L);
-%             disp('iterative, guess = ');disp(guess)
+            [PHIs,L] = eigs((A),(B),n_curves,guess);
+            L = diag(L);
         catch
             if guess == guess1
                 guess = guess2;
             else
                 guess = guess1;
             end
-            [PHIs,L] = eigs((A),(B),n_curves,guess);L = diag(L);
+            [PHIs,L] = eigs((A),(B),n_curves,guess);
+            L = diag(L);
             disp('iterative, guess = ');disp(guess)
         end
-%             disp('iterative, guess = ');disp(guess)
-%             [PHIs,L] = eigs((A),(B),n_curves,guess);L = diag(L);
-            
-        
     end
     
-% full_eig
-
     % post process solutions     
     % gamma-X direction
     L_tol = 1e-9;
     L_tol = 1e-99;
-%     L(abs(L)<L_tol) = nan + 1i*nan;
     logL = log(L');
     Lsave(:,j1) = L;
-    
-%     vs = [logL;zeros(n_p-1,length(L))]/(-1i);
-%     kapvecs = R(1:n_p,1:n_p)\vs;
-%     kappas = kapvecs(1,:);
     kappas = logL/(-1i*R(1));
-    
-%     if rem(j1,20) == 0
-%         figure(212);clf
-%         subplot(1,2,1)
-%         plot3(real(Lsave),imag(Lsave),omegas,'r.')
-%         subplot(1,2,2)
-%         plot3(real(kappas),imag(kappas),omegas,'b.')
-%         drawnow
-%         pause(0.01)
-%     end
-    % remove infinite kappa values by setting to nan
-%     kappas(isinf(abs(kappas))) = nan + 1i*nan;
     
     % sort solutions by complex magnitude
     [~,i_sort] = sort(abs(kappas));
     kappas = kappas(i_sort);
     PHIs = PHIs(1:n_dof_per,i_sort);
-%     PHIs = PHIs(1:n_dof_per,1:n_curves);
-% size(PHIs)
-% size(PHI)
-% n_dof_per
-% n_dof
     
     % Save solutions
     PHI(:,:,j1) = PHIs;
     kappa(:,j1) = kappas';
     lam(:,j1) = (L);
-    
-%     if rem(j1,20) == 0
-%         figure(212);clf
-%         subplot(1,2,1)
-%         plot3(real(Lsave),imag(Lsave),omegas,'r.')
-%         xlim([-3,3]);ylim([-3,3])
-%         subplot(1,2,2)
-%         
-%         plot3(real(kappa),imag(kappa),omegas,'b.')
-% %         xlim([-3,3]);ylim([-3,3])
-%         drawnow
-%         pause(0.01)
-%         pause
-%         
-%     end   
     
     % loop timing
     t_wloop(j1) = toc(tstart);    
